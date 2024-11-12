7eff3f85360145d730618445c19073b54d2c7f40
Miguel Gonzalez <mgonzalez@uw.co.uk>
2024-11-12 14:11:20 +0000

    Local bootstrap

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
                 target: 'http://localhost:8080/',
                 changeOrigin: true,
                 rewrite: path => path.replace(/^\/api/, '')
+            },
+            '/bootstrap.min.css': {
+                target: 'http://localhost:8080/',
+                changeOrigin: true
             }
         }
 
